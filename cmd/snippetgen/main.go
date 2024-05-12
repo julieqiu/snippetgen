d752620032943e97ac1739a6dbf9d13f4848b1b4
Julie Qiu <julie@golang.org>
2024-05-12 17:56:27 -0400

    generated: updated

--- cmd/snippetgen/main.go
+++ cmd/snippetgen/main.go
@@ -15,49 +15,33 @@ func main() {
 }
 
 const (
-	inputDir = "/Users/julieqiu/code/julieqiu/snippetgen/testdata/googleapis"
-	root     = "/Users/julieqiu/code/julieqiu/snippetgen"
+	root = "/Users/julieqiu/code/julieqiu/snippetgen"
 )
 
 var (
-	files = []string{
-		inputDir + "/google/api/annotations.proto",
-		inputDir + "/google/api/client.proto",
-		inputDir + "/google/api/field_behavior.proto",
-		inputDir + "/google/api/http.proto",
-		inputDir + "/google/api/launch_stage.proto",
-		inputDir + "/google/api/resource.proto",
-		inputDir + "/google/cloud/secretmanager/v1/resources.proto",
-		inputDir + "/google/cloud/secretmanager/v1/service.proto",
-		inputDir + "/google/iam/v1/iam_policy.proto",
-		inputDir + "/google/iam/v1/logging/audit_data.proto",
-		inputDir + "/google/iam/v1/options.proto",
-		inputDir + "/google/iam/v1/policy.proto",
-		inputDir + "/google/type/expr.proto",
+	googleapisDir = fmt.Sprintf("%s/testdata/googleapis", root)
+	outputDir     = fmt.Sprintf("%s/generated", root)
+	files         = []string{
+		fmt.Sprintf("%s/google/cloud/secretmanager/v1/resources.proto", googleapisDir),
+		fmt.Sprintf("%s/google/cloud/secretmanager/v1/service.proto", googleapisDir),
 	}
-	opts = map[string]string{
-		"go-gapic-package":    "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb;secretmanagerpb",
-		"grpc-service-config": inputDir + "/google/cloud/secretmanager/v1/secretmanager_grpc_service_config.json",
-		"api-service-config":  inputDir + "/google/cloud/secretmanager/v1/secretmanager_v1.yaml",
+	options = []string{
+		"go-gapic-package=cloud.google.com/go/secretmanager/apiv1/secretmanagerpb;secretmanagerpb",
+		fmt.Sprintf("api-service-config=%s/google/datastore/v1/datastore_v1.yaml", googleapisDir),
+		fmt.Sprintf("api-service-config=%s/google/cloud/secretmanager/v1/secretmanager_v1.yaml", googleapisDir),
+		fmt.Sprintf("grpc-service-config=%s/google/cloud/secretmanager/v1/secretmanager_grpc_service_config.json", googleapisDir),
 	}
 )
 
 func run() error {
-	var parameters []string
-	for k, v := range opts {
-		parameters = append(parameters, fmt.Sprintf("%s=%s", k, v))
-	}
-
 	cmd := exec.Command(
 		"protoc",
-		"-I",
-		inputDir,
-		"--go_gapic_out",
-		root+"/generated",
-		"--go_gapic_opt",
-		"'go-gapic-package=go-gapic-package=cloud.google.com/go/secretmanager/apiv1/secretmanagerpb;secretmanagerpb,grpc-service-config=/Users/julieqiu/code/julieqiu/snippetgen/testdata/googleapis/google/cloud/secretmanager/v1/secretmanager_grpc_service_config.json,api-service-config=/Users/julieqiu/code/julieqiu/snippetgen/testdata/googleapis/google/cloud/secretmanager/v1/secretmanager_v1.yaml'",
-		strings.Join(files, " "),
+		"--go_gapic_out", outputDir,
+		"--go_gapic_opt", fmt.Sprintf("'%s'", strings.Join(options, ",")),
+		"-I", googleapisDir,
+		fmt.Sprintf("%s/google/cloud/secretmanager/v1/*.proto", googleapisDir),
 	)
+
 	fmt.Println(cmd.String())
 
 	cmd.Stdout = os.Stdout
--- go.mod
+++ go.mod
@@ -4,36 +4,24 @@ go 1.21.1
 
 require (
 	cloud.google.com/go/iam v1.1.8
-	cloud.google.com/go/secretmanager v1.13.0
-	github.com/googleapis/gax-go/v2 v2.12.4
-	google.golang.org/api v0.180.0
-	google.golang.org/grpc v1.63.2
+	cloud.google.com/go/longrunning v0.5.6
+	github.com/ghodss/yaml v1.0.0
+	github.com/google/go-cmp v0.6.0
+	gitlab.com/golang-commonmark/markdown v0.0.0-20211110145824-bf3e522c626a
+	google.golang.org/genproto v0.0.0-20240401170217-c3f982113cda
+	google.golang.org/genproto/googleapis/api v0.0.0-20240429193739-8cf5692501f6
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240429193739-8cf5692501f6
 	google.golang.org/protobuf v1.34.1
 )
 
 require (
-	cloud.google.com/go/auth v0.4.1 // indirect
-	cloud.google.com/go/auth/oauth2adapt v0.2.2 // indirect
-	cloud.google.com/go/compute/metadata v0.3.0 // indirect
-	github.com/go-logr/logr v1.4.1 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/google/s2a-go v0.1.7 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
-	go.opencensus.io v0.24.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
-	go.opentelemetry.io/otel v1.24.0 // indirect
-	go.opentelemetry.io/otel/metric v1.24.0 // indirect
-	go.opentelemetry.io/otel/trace v1.24.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	gitlab.com/golang-commonmark/html v0.0.0-20191124015941-a22733972181 // indirect
+	gitlab.com/golang-commonmark/linkify v0.0.0-20191026162114-a0c2df6c8f82 // indirect
+	gitlab.com/golang-commonmark/mdurl v0.0.0-20191124015652-932350d1cb84 // indirect
+	gitlab.com/golang-commonmark/puny v0.0.0-20191124015043-9f83538fa04f // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/oauth2 v0.20.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
-	golang.org/x/time v0.5.0 // indirect
-	google.golang.org/genproto v0.0.0-20240401170217-c3f982113cda // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240429193739-8cf5692501f6 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240429193739-8cf5692501f6 // indirect
+	google.golang.org/grpc v1.63.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
